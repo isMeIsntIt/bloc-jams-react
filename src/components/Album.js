cb9694ae61ea6f18ff8594bde8fefabd339fd04b
Jeremy <goodcleanh8@gmail.com>
2019-04-21 15:00:51 -0600

    Check point complete. Added ReactRouter Links to albums

--- src/components/Album.js
+++ src/components/Album.js
@@ -4,7 +4,7 @@ class Album extends Component {
     render() {
         return (
             <section className="album">
-                Album will go here
+                {this.props.match.params.slug} Album will go here
             </section>
         );
     }
