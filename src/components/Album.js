08402b44dd8588a405122707cda3031f01377696
Jeremy <goodcleanh8@gmail.com>
2019-05-05 12:37:39 -0600

    work in progress

--- src/components/Album.js
+++ src/components/Album.js
@@ -15,13 +15,32 @@ class Album extends Component {
             isPlaying: false
         };
 
-        this.audioElement = document.createElement('audio')
+        this.audioElement = document.createElement('audio');
         this.audioElement.src = album.songs[0].audioSrc;
     }
 
     play() {
         this.audioElement.play();
         this.setState({ isPlaying: true });
+    }
+
+    pause() {
+        this.audioElement.pause();
+        this.setState({ isPlaying: false });
+    }
+
+    setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState({ currentSong: song });
+    }
+
+    handleSongClick(song) {
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong) {
+            this.pause();
+        } else {
+            this.play();
+        }
     }
 
     render() {
